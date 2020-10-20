5697bee50c7a7203837bad05a7f9e4f1d4a30cd0
Lành Đào <tung75397@st.vimaru.edu.vn>
2020-10-20 13:08:04 +0700

    Updates

---
+++ precache-manifest.3073b4d3af19ed7ce17306ed6d3fc07d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "383f31a76c60f3c24a1772764e477fe2",
+    "url": "/baitapled/index.html"
+  },
+  {
+    "revision": "b3eece52412204d563bd",
+    "url": "/baitapled/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "d84e23d2e55ac2c8b2d2",
+    "url": "/baitapled/static/js/2.b3e0fc3b.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/baitapled/static/js/2.b3e0fc3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3eece52412204d563bd",
+    "url": "/baitapled/static/js/main.eca70a98.chunk.js"
+  },
+  {
+    "revision": "01c502d2040b65316fe8",
+    "url": "/baitapled/static/js/runtime-main.36d272ed.js"
+  }
+]);
